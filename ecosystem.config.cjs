df17e84cc104813dca9c8f7d9cd7eb9fc59fd689
Ruslan Belchenkov <r.belchenkov@litefinance.com>
2024-10-01 10:54:04 +0300

    Changes

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'Macroprint',
-            port: '4206',
+            port: '4210',
             exec_mode: 'cluster',
             instances: 'max',
             script: './.output/server/index.mjs'
