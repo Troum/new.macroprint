4fbf173601d270e89b00fe72b3bd533882c210be
Ruslan Belchenkov <r.belchenkov@litefinance.com>
2024-10-01 10:53:42 +0300

    Changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,7 @@ export default defineNuxtConfig({
         }
     },
     devtools: {enabled: true},
+    compatibilityDate: '2024-09-01',
     hooks: {
         'pages:extend'(pages) {
             pages.push({
@@ -40,9 +41,9 @@ export default defineNuxtConfig({
         apikey: '0845768b-3f33-453f-970b-be8e7b249bd7',
     },
     sanity: {
-        projectId: process.env.SANITY_PROJECT_ID || 'gtsnokwt',
+        projectId: process.env.SANITY_PROJECT_ID,
         useCdn: false,
-        token: process.env.SANITY_API_TOKEN || 'skD5EFFZKlHTYktk8w5sQut95EISGvF0W0IyYBqKPGfg3oD9s4worPgHIANUAqfsPnnfiznq1zaKgaIRVdTz5p6EsMQyAr2ypyPD2GXKQyDO4xCcQ1pna3Aqg6wOmNOnsTYrmVjbnivfGK4Hd5FsOIFS06aptqFpowLfrTbGq3tkvcRIrv6s',
+        token: process.env.SANITY_API_TOKEN,
         ignoreBrowserTokenWarning: true
     },
     image: {
